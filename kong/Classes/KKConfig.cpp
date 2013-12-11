7813924bfe617f1a53d13a9bbd988670fd11c0b6
Shura.Liu <liuxiaoyi@verycd.com>
2013-12-11 21:46:24 +0800

    修改配置

--- kong/Classes/KKConfig.cpp
+++ kong/Classes/KKConfig.cpp
@@ -7,5 +7,3 @@
 
 #include "KKConfig.h"
 
-const char* KK_KEY_MUSIC_ON = "kk_key_music_on";
-const char* KK_KEY_AUDIO_ON = "kk_key_audio_on";
--- kong/Classes/KKConfig.h
+++ kong/Classes/KKConfig.h
@@ -8,7 +8,7 @@
 #ifndef KKCONFIG_H_
 #define KKCONFIG_H_
 
-static const char* KK_KEY_MUSIC_ON;
-static const char* KK_KEY_AUDIO_ON;
+static const char* KK_KEY_MUSIC_ON = "kk_key_music_on";
+static const char* KK_KEY_AUDIO_ON = "kk_key_audio_on";
 
 #endif /* KKCONFIG_H_ */
