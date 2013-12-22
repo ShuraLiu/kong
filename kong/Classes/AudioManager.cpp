a700e8553613fe46a1be1ae21602938fb51e3f31
Shura.Liu <liuxiaoyi@verycd.com>
2013-12-22 16:59:45 +0800

    增加声音管理类的通知

--- kong/Classes/AudioManager.cpp
+++ kong/Classes/AudioManager.cpp
@@ -40,3 +40,46 @@ void AudioManager::turnOffMusic()
 	CCUserDefault::sharedUserDefault()->setBoolForKey(KK_KEY_MUSIC_ON, false);
 }
 
+bool AudioManager::isAudioOn() const
+{
+	return CCUserDefault::sharedUserDefault()->getBoolForKey(KK_KEY_AUDIO_ON, true);
+}
+
+bool AudioManager::isMusicOn() const
+{
+	return CCUserDefault::sharedUserDefault()->getBoolForKey(KK_KEY_MUSIC_ON, true);
+}
+
+void AudioManager::addObserver(AudioObserver* obs)
+{
+	if(observers_.empty() || observers_.end() == std::find(observers_.begin(), observers_.end(), obs))
+	{
+		observers_.push_back(obs);
+	}
+}
+
+void AudioManager::removeObserver(AudioObserver* obs)
+{
+	if(!observers_.empty())
+	{
+		AudioObserverList::iterator it = std::find(observers_.begin(), observers_.end(), obs);
+		if(it != observers_.end())
+		{
+			observers_.erase(it);
+		}
+	}
+}
+
+void AudioManager::notifyObservers(AudioNotifyType type, bool isOn)
+{
+	if(!observers_.empty())
+	{
+		AudioObserverList::iterator it = observers_.begin();
+		while(it != observers_.end())
+		{
+			(*it)->onAudioNotify(type, isOn);
+			++it;
+		}
+	}
+}
+
--- kong/Classes/AudioManager.h
+++ kong/Classes/AudioManager.h
@@ -8,6 +8,23 @@
 #ifndef AUDIOMANAGER_H_
 #define AUDIOMANAGER_H_
 
+#include "cocos2d.h"
+
+enum AudioNotifyType
+{
+	MUSIC_OPTION = 0,
+	AUDIO_OPTION,
+};
+
+class AudioObserver
+{
+public:
+	AudioObserver(){};
+	virtual ~AudioObserver(){};
+
+	virtual void onAudioNotify(AudioNotifyType type, bool isOn) = 0;
+};
+
 class AudioManager
 {
 public:
@@ -30,6 +47,15 @@ public:
 		static AudioManager manager;
 		return manager;
 	}
+
+	void addObserver(AudioObserver* obs);
+	void removeObserver(AudioObserver* obs);
+	void notifyObservers(AudioNotifyType type, bool isOn);
+
+private:
+	typedef std::vector<AudioObserver*> AudioObserverList;
+
+	AudioObserverList observers_;
 };
 
 #endif /* AUDIOMANAGER_H_ */
